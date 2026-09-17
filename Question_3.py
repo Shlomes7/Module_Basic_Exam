7f32abb9647d7bbbb4ee3def37cc972e6cb92f9d
Shlomes7 <Arielkupy@gmail.com>
2026-09-17 19:40:51 +0300

    q3

--- Question_3.py
+++ Question_3.py
@@ -1,6 +1,14 @@
 def camel_to_hyphen(text: str) -> str:
-    for l in text:
-        if l.isupper():
-            text.insert('-')
-    return 'text'
-print(camel_to_hyphen('PytHone'))
+    l1 = list(text)
+    place_count = 0
+    for letter in l1:
+        if letter.isupper():
+            l1[place_count] = letter.lower()
+            l1.insert(place_count, '-')
+        place_count += 1
+    return ''.join(l1)
+
+print(camel_to_hyphen('helloPython'))
+print(camel_to_hyphen('myVariableName'))
+print(camel_to_hyphen('python'))
+print(camel_to_hyphen('aBigTest'))
